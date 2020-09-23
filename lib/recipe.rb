a64cfcc2d0a4e8e6ee3a23f69755e8dfa852ec27
Eduardo Parra <helloeduardo@icloud.com>
2020-09-23 09:27:24 -0600

    Add ingredients and test

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,7 +1,20 @@
 class Recipe
-  attr_reader :name
+  attr_reader :name, :ingredients_required
 
   def initialize(name)
     @name = name
+    @ingredients_required = {}
+  end
+
+  def add_ingredient(ingredient, amount)
+    if ingredients_required[ingredient]
+      @ingredients_required[ingredient] += amount
+    else
+      @ingredients_required[ingredient] ||= amount
+    end
+  end
+
+  def ingredients
+    ingredients_required.keys
   end
 end
--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -9,4 +9,25 @@ class RecipeTest < Minitest::Test
 
     assert_instance_of Recipe, recipe1
   end
+
+  def test_it_can_add_ingredients
+    recipe1 = Recipe.new("Mac and Cheese")
+
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+
+    assert_equal ({}), recipe1.ingredients_required
+
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+
+    expected = {
+      ingredient1 => 6,
+      ingredient2 => 8
+    }
+
+    assert_equal expected, recipe1.ingredients_required
+    assert_equal [ingredient1, ingredient2], recipe1.ingredients
+  end
 end
