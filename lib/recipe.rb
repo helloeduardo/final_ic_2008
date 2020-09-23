5849d7289153961580812e242dbcd99a45883f18
Eduardo Parra <helloeduardo@icloud.com>
2020-09-23 09:41:06 -0600

    Add total calories and test

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -3,18 +3,20 @@ class Recipe
 
   def initialize(name)
     @name = name
-    @ingredients_required = {}
+    @ingredients_required = Hash.new(0)
   end
 
   def add_ingredient(ingredient, amount)
-    if ingredients_required[ingredient]
-      @ingredients_required[ingredient] += amount
-    else
-      @ingredients_required[ingredient] ||= amount
-    end
+    @ingredients_required[ingredient] += amount
   end
 
   def ingredients
     ingredients_required.keys
+  end
+
+  def total_calories
+    ingredients_required.sum do |ingredient, amount|
+      ingredient.calories * amount
+    end
   end
 end
