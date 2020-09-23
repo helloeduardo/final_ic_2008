539a5d812290174ca3799ed9c49be97179a477db
Eduardo Parra <helloeduardo@icloud.com>
2020-09-23 09:09:45 -0600

    Add pantry and test

---
+++ test/pantry_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+
+class PantryTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    pantry = Pantry.new
+
+    assert_instance_of Pantry, pantry
+  end
+end
